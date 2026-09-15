a77d08130f57490b3823b34c9521af8d4d4d63a9
AVHands <annievhands@gmail.com>
2026-09-15 14:33:12 +0000

    committing :)

---
+++ FS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+struct FNT{
+    
+};
+void createFS(int blocks = 0) //probably should be a struct lol
+{}
+//void formatFS ()
+void saveFS(char name[])
+{}
+void openFS(char name[])
+
+int main()
+{
+    return(0);
+}
